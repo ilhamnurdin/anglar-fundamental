c050343c310ba111615eb99b11cb958e7817ceef
ilham <ilhamnurdin15@gmail.com>
2021-08-11 20:18:47 +0700

    Define Variable, Experession and Data Binding

--- testing-ma-bro/src/app/app.component.ts
+++ testing-ma-bro/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testing-ma-bro';
+  title = 'Ini Title ma bro';
+  namaDepan = 'ashiap';
+  namaBelakang:string='sampah';
+  umur:number=100;
+  kumpulanAngka = [0,1,2,3];
+
+  ketikaDiKlik() {
+    this.namaDepan = 'AHHA',
+    this.namaBelakang = 'Sampah sodara-sodara',
+    this.umur = 120
+  }
+
+  username:string = 'Bayufajar';
 }
